85857b820f7efd53b19ddd7e9d61d56c5315eb99
Stefan Negru <stefan@blankdots.com>
2019-02-25 11:23:37 +0200

    add retry logic to script #1

--- test.py
+++ test.py
@@ -11,18 +11,19 @@ from legacryptor.crypt4gh import encrypt, Header, get_header
 import pgpy
 import argparse
 from minio import Minio
-from time import sleep
 import requests
 import filecmp
 import asyncio
 import asyncpg
 import yaml
 from urllib.parse import urlparse
+from retrying import retry
 
 
 FORMAT = '[%(asctime)s][%(name)s][%(process)d %(processName)s][%(levelname)-8s] (L:%(lineno)s) %(funcName)s: %(message)s'
 logging.basicConfig(format=FORMAT, datefmt='%Y-%m-%d %H:%M:%S')
 LOG = logging.getLogger(__name__)
+# change to DEBUG for full output
 LOG.setLevel(logging.INFO)
 
 
@@ -31,7 +32,7 @@ async def get_last_id(db_user, db_name, db_pass, db_host):
     conn = await asyncpg.connect(user=db_user, password=db_pass,
                                  database=db_name, host=db_host)
     values = await conn.fetchrow('''SELECT created_at, id FROM local_ega.files ORDER BY created_at DESC LIMIT 1''')
-    LOG.info(f"Database ID: {values['id']}")
+    LOG.debug(f"Database ID: {values['id']}")
     return values['id']
     await conn.close()
 
@@ -44,7 +45,7 @@ async def file2dataset_map(db_user, db_name, db_pass, db_host, file_id, dataset_
     await conn.execute('''
         INSERT INTO local_ega.file2dataset(id, file_id, dataset_id) VALUES($1, $2, $3)
     ''', last_index['id'] + 1 if last_index else 1, file_id, dataset_id)
-    LOG.info(f"Mapped ID: {file_id} to Dataset: {dataset_id}")
+    LOG.debug(f"Mapped ID: {file_id} to Dataset: {dataset_id}")
     await conn.close()
 
 
@@ -55,7 +56,7 @@ def open_ssh_connection(hostname, user, key_path, key_pass='password', port=2222
         k = paramiko.RSAKey.from_private_key_file(key_path, password=key_pass)
         client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         client.connect(hostname, allow_agent=False, look_for_keys=False, port=port, timeout=0.3, username=user, pkey=k)
-        LOG.info(f'ssh connected to {hostname}:{port} with {user}')
+        LOG.info(f'ssh connected to {hostname}:{port} with {user} | PASS |')
     except paramiko.BadHostKeyException as e:
         LOG.error(f'Something went wrong {e}')
         raise Exception('BadHostKeyException on ' + hostname)
@@ -75,11 +76,11 @@ def sftp_upload(hostname, user, file_path, key_path, key_pass='password', port=2
         k = paramiko.RSAKey.from_private_key_file(key_path, password=key_pass)
         transport = paramiko.Transport((hostname, port))
         transport.connect(username=user, pkey=k)
-        LOG.info(f'sftp connected to {hostname}:{port} with {user}')
+        LOG.debug(f'sftp connected to {hostname}:{port} with {user}')
         sftp = paramiko.SFTPClient.from_transport(transport)
         filename, _ = os.path.splitext(file_path)
         sftp.put(file_path, f'{filename}.c4ga')
-        LOG.info(f'file uploaded {filename}.c4ga')
+        LOG.info(f'file uploaded {filename}.c4ga | PASS |')
     except Exception as e:
         LOG.error(f'Something went wrong {e}')
         raise e
@@ -102,12 +103,13 @@ def submit_cega(address, user, vhost, message, routing_key, mq_password, correla
                                                               delivery_mode=2))
 
         connection.close()
-        LOG.info(f'Message published to CentralEGA: {message}')
+        LOG.debug(f'Message published to CentralEGA: {message}')
     except Exception as e:
         LOG.error(f'Something went wrong {e}')
         raise e
 
 
+@retry(stop_max_attempt_number=10)
 def get_corr(address, user, vhost, queue, filepath, mq_password, latest_message=True, port=5672):
     """Read all messages from a queue and fetches the correlation_id for the one with given path, if found."""
     mq_address = f'amqp://{user}:{mq_password}@{address}:{port}/{vhost}'
@@ -155,7 +157,7 @@ def get_corr(address, user, vhost, queue, filepath, mq_password, latest_message=
         for cid, mid in correlation_ids:
             if mid > message_id:
                 correlation_id = cid
-    LOG.info(f'correlation_id: {correlation_id}')
+    LOG.debug(f'correlation_id: {correlation_id}')
     return correlation_id
 
 
@@ -171,7 +173,7 @@ def encrypt_file(file_path, pubkey):
         encrypt(pubkey, infile, file_size, open(f'{output_base}.c4ga', 'wb'))
         with open(output_file, 'rb') as read_file:
             c4ga_md5 = md5(read_file.read()).hexdigest()
-        LOG.info(f'File {output_base}.c4ga is the encrypted file with md5: {c4ga_md5}.')
+        LOG.debug(f'File {output_base}.c4ga is the encrypted file with md5: {c4ga_md5}.')
     except Exception as e:
         LOG.error(f'Something went wrong {e}')
         raise e
@@ -188,20 +190,44 @@ def strip_scheme(url):
     return parsed.geturl().replace(scheme, '', 1)
 
 
+@retry(stop_max_attempt_number=10)
 def list_s3_objects(minio_address, bucket_name, region_name, file_id, access, secret):
     """Check if there is a file inside s3."""
     minioClient = Minio(strip_scheme(minio_address), access_key=access, secret_key=secret,
                         region=region_name, secure=False)
-    LOG.info(f'Connected to S3: {minio_address}.')
+    LOG.debug(f'Connected to S3: {minio_address}.')
     # List all object paths in bucket that begin with my-prefixname.
     objects = minioClient.list_objects_v2(bucket_name, recursive=True)
     object_list = [obj.object_name for obj in objects]
-    assert str(file_id) in object_list, f"Could not find the file just uploaded!"
-    LOG.info(f"Found the file uploaded to inbox as {file_id} in S3Storage.")
-    # all_objects = minioClient.list_objects(bucket_name, recursive=True)
-    # LOG.info("All the files in Lega bucket: ")
-    # for obj in all_objects:
-    #     LOG.info(f'Found ingested file: {obj.object_name} of size: {obj.size}.')
+    assert str(file_id) in object_list, f"Could not find the file just uploaded! | FAIL | "
+    LOG.info(f"Found the file uploaded to inbox as {file_id} in S3Storage. | PASS |")
+    all_objects = minioClient.list_objects(bucket_name, recursive=True)
+    LOG.debug("All the files in Lega bucket: ")
+    for obj in all_objects:
+        LOG.debug(f'Found ingested file: {obj.object_name} of size: {obj.size}.')
+
+
+def download_to_file(service, payload, output, headers=None):
+    """Download file from service and write to file."""
+    download = requests.get(service, params=payload, headers=headers)
+    # We are using filecmp thus we will write content to file
+    assert download.status_code == 200, f'We got a status that is not OK {download.status_code} | FAIL |'
+    LOG.info(f"File downloaded from {service}. | PASS |")
+    LOG.debug(f'write content to {output}')
+    open(output, 'wb').write(download.content)
+
+
+def compare_files(service, downloaded_file, used_file):
+    """Compare Downloaded file with original."""
+    LOG.debug(f'Comparing downloaded via {service} file with original file ...')
+    # comparing content of the files
+    assert filecmp.cmp(downloaded_file, used_file, shallow=False), 'Files are not equal. | FAIL | '
+    # The low level alternative would be:
+    # with open(res_file) as f1:
+    #     with open(used_file) as f2:
+    #         if f1.read() == f2.read():
+    #             pass
+    LOG.info(f'{service} Downloaded file is equal to the original file. | PASS |')
 
 
 def main():
@@ -251,49 +277,37 @@ def main():
     if c4ga_md5:
         sftp_upload(config['inbox_address'], test_user, test_file, key_pk, port=int(config['inbox_port']))
         correlation_id = get_corr(config['cm_address'], config['cm_user'],
-                                  config['cm_vhost'], 'v1.files.inbox', test_file, config['cm_pass'])
+                                  config['cm_vhost'], 'v1.files.inbox', test_file, config['cm_pass'],
+                                  port=config['cm_port'])
         submit_cega(config['cm_address'], config['cm_user'], config['cm_vhost'],
                     {'user': test_user, 'filepath': test_file}, 'files',
-                    config['cm_pass'], correlation_id)
-        # wait for submission to go through - might need to be longer depending on the file
-        # proper retry should be implemented
-        sleep(30)
+                    config['cm_pass'], correlation_id, port=config['cm_port'])
         # Once the file has been ingested it should be the last ID in the database
         # We use this ID everywhere including donwload from DataEdge
         # In future versions once we fix DB schema we will use StableID for download
         fileID = loop.run_until_complete(get_last_id(config['db_user'], config['db_name'],
                                                      config['db_pass'], config['db_address']))
+        # wait for submission to go through - might need to be longer depending on the file
+        # proper retry should be implemented
+        get_corr(config['cm_address'], config['cm_user'],
+                 config['cm_vhost'], 'v1.files.completed', test_file, config['cm_pass'],
+                 port=config['cm_port'])
         # Stable ID should be sent by CentralEGA
         submit_cega(config['cm_address'], config['cm_user'], config['cm_vhost'],
                     {'file_id': fileID, 'stable_id': f'EGAF{stableID}'}, 'stableIDs',
-                    config['cm_pass'], correlation_id)
-        # wait for the file in s3
-        # proper retry should be implemented
-        sleep(30)
+                    config['cm_pass'], correlation_id, port=config['cm_port'])
         list_s3_objects(config['s3_address'], config['s3_region'],
                         config['s3_bucket'], fileID,
                         config['s3_access'], config['s3_secret'])
-    LOG.info('Ingestion DONE')
-    LOG.info('-------------------------------------')
+    LOG.debug('Ingestion DONE')
+    LOG.debug('-------------------------------------')
     # Verify that the file can be downloaded from RES using the session_key and IV
-    payload = {'sourceKey': session_key, 'sourceIV': iv, 'filePath': fileID}
-    res_url = f"http://{config['res_address']}:{config['res_address']}/file"
-    download = requests.get(res_url, params=payload)
-    # We are using filecmp thus we will write content to file
-    assert download.status_code == 200, f'We got a different status from RES {download.status_code}'
-    LOG.info(f'write content to {res_file}')
-    open(res_file, 'wb').write(download.content)
-    LOG.info('Comparing RES downloaded file with original file ...')
-    # comparing content of the files
-    assert filecmp.cmp(res_file, used_file, shallow=False), 'files are not equal'
-    # The low level alternative would be:
-    # with open(res_file) as f1:
-    #     with open(used_file) as f2:
-    #         if f1.read() == f2.read():
-    #             pass
-    LOG.info('RES Downloaded file is equal to the original file.')
+    res_payload = {'sourceKey': session_key, 'sourceIV': iv, 'filePath': fileID}
+    res_url = f"http://{config['res_address']}:{config['res_port']}/file"
+    download_to_file(res_url, res_payload, res_file)
+    compare_files('RES', res_file, used_file)
 
-    LOG.info('Mapping file to dataset for retrieving file via dataedge.')
+    LOG.debug('Mapping file to dataset for retrieving file via dataedge.')
 
     # There is no component asigning permissions for files in datasets
     # Thus we need this step
@@ -308,21 +322,12 @@ def main():
     edge_payload = {'destinationFormat': 'plain'}
     edge_headers = {'Authorization': f'Bearer {token}'}  # No token no permissions
     dataedge_url = f"http://{config['dataedge_address']}:{config['dataedge_port']}/files/{fileID}"
-    down_dataedge = requests.get(dataedge_url, params=edge_payload, headers=edge_headers)
-    LOG.info(down_dataedge.url)
+    download_to_file(dataedge_url, edge_payload, dataedge_file, edge_headers)
+    compare_files('DataEdge', dataedge_file, used_file)
 
-    # 206 would be a partial content that means we misshandle some bytes
-    assert down_dataedge.status_code == 200, f'We got a different status from DataEdge {down_dataedge.status_code}'
-    LOG.info(f'write content to {dataedge_file}')
-    open(dataedge_file, 'wb').write(down_dataedge.content)
-    LOG.info('Comparing Dataedge downloaded file with original file ...')
-    # comparing content of the files
-    assert filecmp.cmp(dataedge_file, used_file, shallow=False), 'files are not equal'
-    LOG.info('Dataedge Downloaded file is equal to the original file.')
-
-    LOG.info('Outgestion DONE')
-    LOG.info('-------------------------------------')
-    LOG.info('Should be all!')
+    LOG.debug('Outgestion DONE')
+    LOG.debug('-------------------------------------')
+    LOG.debug('Should be all!')
 
 
 if __name__ == '__main__':
