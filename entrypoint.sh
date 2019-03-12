fd8cc3768803797d7fc8811061aa80cb97023400
Jocke <j.bygdell@gmail.com>
2019-03-12 12:16:37 +0100

    Change to a smaller test file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-wget -O test_file ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase3/data/HG00458/alignment/HG00458.unmapped.ILLUMINA.bwa.CHS.low_coverage.20130415.bam
+wget -O test_file ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase3/data/HG00100/sequence_read/ERR013140.filt.fastq.gz
 
 sleep 3
 
