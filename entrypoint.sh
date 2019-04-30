9acae373608dcf881461f06d0f478af27dbce0e0
Joakim Bygdell <joakim.bygdell@nbis.se>
2019-04-30 08:01:17 +0200

    Use unique name for each file uploaded
    This way we can make sure that the ingested file is the one we uploaded and not something that already existed in the inbox/s3 bucket.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
-wget -O test_file ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase3/data/HG00100/sequence_read/ERR013140.filt.fastq.gz
+test_file=$(date +"%Y-%m-%d_%H-%M-%S")
+wget -O $test_file ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase3/data/HG00100/sequence_read/ERR013140.filt.fastq.gz
 
 sleep 3
 
-python test.py test_file /conf/config.yaml
+python test.py $test_file /conf/config.yaml
