39b17dd00be26adca9ff8856d90e716543b8d848
Joakim Bygdell <joakim.bygdell@nbis.se>
2019-05-08 11:01:02 +0200

    Enable selection of file size to use in the test.
    Small = 5MB, medium =  6GB, large = 41GB

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
+
+if [ "$1" = 'small' ]; then     
+    FILE="ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase3/data/HG00100/sequence_read/ERR013140.filt.fastq.gz"
+elif [ "$1" = "medium" ]; then
+    FILE="ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase3/data/HG00100/sequence_read/SRR099966_1.filt.fastq.gz"
+elif [ "$1" = "large" ]; then
+    FILE="ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase3/data/HG00100/alignment/HG00100.mapped.ILLUMINA.bwa.GBR.low_coverage.20130415.bam"
+else
+    echo "file size not set"
+    exit 1
+fi
+
 test_file=$(date +"%Y-%m-%d_%H-%M-%S")
-wget -O $test_file ftp://ftp.1000genomes.ebi.ac.uk/vol1/ftp/phase3/data/HG00100/sequence_read/ERR013140.filt.fastq.gz
+wget -O /volume/$test_file $FILE
 
 sleep 3
 
-python test.py $test_file /conf/config.yaml
+python test.py /volume/$test_file /conf/config.yaml
