5398e83cf51a13b03ce92dfb3f95cd69535144dc
Stefan Negru <stefan@blankdots.com>
2019-07-12 14:33:40 +0300

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='lega_tester',
-    version='0.2.0',
+    version='0.3.0',
     packages=find_packages(),
     py_modules=['lega_tester'],
     include_package_data=True,
